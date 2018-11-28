fdfbdf3d0fbb9a0ab6761675dc6492682cbc283d
Juan Camilo Rodriguez <juancamilo9607@gmail.com>
2018-11-28 10:36:55 -0500

    Se agrego bootstrap al proyecto

--- src/App.js
+++ src/App.js
@@ -7,12 +7,30 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <input type="text"></input>
-          <br/>
-          <input type="text"></input>
-          <br/>
-          <button>INGRESAR</button>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+            <img src={logo} className="App-logo" alt="logo" />
+            </div>
+          
+            <div class="col-sm-12">   
+            <input type="email" class="form-control" placeholder="Email"></input>
+            </div>
+            <div class="col-sm-12">
+            <input type="password" class="form-control" placeholder="Contraseña"></input>
+            </div>
+          </div>
+        </div>
+        <br/>
+        <div class="container">
+          <div class="row"> 
+            <div class="col-sm-12">
+            <button type="button" class="btn btn-block">INGRESAR</button>
+            </div>
+          </div> 
+        </div>
+          
+        
           <a
             className="App-link"
             href="www.grupoasistencia.com"
@@ -22,9 +40,16 @@ class App extends Component {
             ¿Olvidaste tu contraseña?
           </a>
           <br/>
-          <button>Sing in</button>
-          <br/>
-          <button>Sing in</button>
+          <div class="container">
+          <div class="row">
+          <div class="col-6">
+          <button type="button" class="btn btn-primary btn-block"><i class="fab fa-facebook-square"></i> Sing in</button>
+          </div>
+          <div class="col-6">
+          <button type="button" class="btn btn-danger btn-block"><i class="fab fa-google-plus-g"></i> Sing in</button>
+          </div>
+          </div>
+          </div>
           <br/>
           <span>¿Aún no tienes cuenta? <a
             className="App-link"
